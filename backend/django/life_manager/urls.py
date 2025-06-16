3c537d38d0627a34ad1f11b86ebbfce531cbc611
OCP <oscarcarballopuebla@gmail.com>
2025-06-17 00:17:40 +0200

    healthcheck and pytest on precommit

--- backend/django/life_manager/urls.py
+++ backend/django/life_manager/urls.py
@@ -1,8 +1,14 @@
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
 from django.urls import include, path
 
-urlpatterns = [path("", include("tasks.urls"))]
+
+def health_check(request):
+    return HttpResponse("OK")
+
+
+urlpatterns = [path("", include("tasks.urls")), path("health/", health_check, name="health_check")]
 
 # Explicitly add static files serving for development
 if settings.DEBUG:
